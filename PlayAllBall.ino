849a23369652cb283ffbc40265a4415e77ec72e5
Ben Schoolcraft <schoolieb44@gmail.com>
2020-01-19 18:17:34 -0500

    Got it to work!

--- PlayAllBall.ino
+++ PlayAllBall.ino
@@ -65,12 +65,7 @@ void setup() {
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
-
-  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
   
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
-
   digitalWrite(MFR_SS, HIGH);
 
   
@@ -104,6 +99,7 @@ void loop() {
   t = millis() / 1000;
 
   tft.print("beans" + String(t));
+  printHexTFT(mfrc522.uid.uidByte, mfrc522.uid.size);
 
   digitalWrite(TFT_SS, HIGH);
 
@@ -115,46 +111,18 @@ void loop() {
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
-
-  Serial.println(F("Ping"));
   
   // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
-  Serial.println(F("Pingdong"));
 
-  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-  Serial.println(mfrc522.PICC_GetTypeName(piccType));
 
- // Check is the PICC of Classic MIFARE type
-  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-    Serial.println(F("Your tag is not of type MIFARE Classic."));
-    return;
+ 
+  // Store NUID into nuidPICC array
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = mfrc522.uid.uidByte[i];
   }
-
-  if (mfrc522.uid.uidByte[0] != nuidPICC[0] || 
-    mfrc522.uid.uidByte[1] != nuidPICC[1] || 
-    mfrc522.uid.uidByte[2] != nuidPICC[2] || 
-    mfrc522.uid.uidByte[3] != nuidPICC[3] ) {
-    Serial.println(F("A new card has been detected."));
-
-    // Store NUID into nuidPICC array
-    for (byte i = 0; i < 4; i++) {
-      nuidPICC[i] = mfrc522.uid.uidByte[i];
-    }
-   
-    Serial.println(F("The NUID tag is:"));
-    Serial.print(F("In hex: "));
-    printHex(mfrc522.uid.uidByte, mfrc522.uid.size);
-    Serial.println();
-    Serial.print(F("In dec: "));
-    printDec(mfrc522.uid.uidByte, mfrc522.uid.size);
-    Serial.println();
-  }
-  else Serial.println(F("Card read previously."));
 
   // Halt PICC
   mfrc522.PICC_HaltA();
@@ -174,6 +142,16 @@ void printHex(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);
+  }
+}
+
+/**
+ * Helper routine to dump a byte array as hex values to Serial. 
+ */
+void printHexTFT(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    tft.print(buffer[i] < 0x10 ? " 0" : " ");
+    tft.print(buffer[i], HEX);
   }
 }
 
